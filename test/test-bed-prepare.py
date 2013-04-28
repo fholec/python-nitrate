3fd5c164b8ce8f18d9286ecab94e19c5ce98b6da
Filip Holec <fholec@redhat.com>
2013-04-28 09:27:02 +0200

    Updated test bed script

--- test/test-bed-prepare.py
+++ test/test-bed-prepare.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 
-import nitrate, qe
-import xmlrpclib
+import nitrate
 import random
 import optparse
-from os import system
 
-master_plan_name = "Master TP"
-product = "RHEL Tests"
-version = "unspecified"
-type = "Function"
-plantype = 1 
-bug = 1000
-category = 702 
-casestatus = 2 
-build = 979 
-product_version = 389 
+MASTER_TP = "Master TP"
+PRODUCT = nitrate.Product(name="RHEL Tests")
+VERSION = nitrate.Version(product=PRODUCT, version="unspecified")
+PLANTYPE = nitrate.PlanType(name="Function")
+CATEGORY = nitrate.Category(category="Regression", product=PRODUCT)
+CASESTATUS = nitrate.CaseStatus("CONFIRMED")
+BUILD = nitrate.Build(product=PRODUCT, build="RHEL6-6.0")
 
-TAGS = {0:"Apps", 1:"Tier 1", 2:"new", 3:"Tier 2", 4:"TIP"}
+TAGS = ["tag{0}".format(id) for id in range(7)]
 
-if __name__ == "__main__":
+def parse_options():
     parser = optparse.OptionParser(
             usage = "./init_tcms [--plans #] [--runs #] [--cases #]")
     parser.add_option("--plans",
@@ -40,39 +35,37 @@ if __name__ == "__main__":
             action = "store",
             default = 1,
             help = "create specified number of cases")
-    (options, arguments) = parser.parse_args()
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    (options, arguments) = parse_options()
 
     # Create master plan (root)
-    master = nitrate.TestPlan(name=master_plan_name, \
-        product=product, version="unspecified", \
-        type=nitrate.PlanType(plantype))
-    master.update()
+    master = nitrate.TestPlan(name=MASTER_TP,
+        product=PRODUCT.name, version=VERSION.name,
+        type=PLANTYPE)
 
-    for i in range(0, options.plans):
+    for plan_count in range(0, options.plans):
         # Create Plans
-        tplan = nitrate.TestPlan(name="Test Plan {0}".format(i+1),\
-            parent=master.id, product=product, version="unspecified",\
-            type=nitrate.PlanType(plantype))
-        tplan.update()
-        for c in range(0, options.cases):
+        testplan = nitrate.TestPlan(name="Test Plan {0}".format(plan_count+1),
+            parent=master.id, product=PRODUCT.name, version=VERSION.name,
+            type=PLANTYPE)
+        for case_count in range(0, options.cases):
             # Create cases
-            case = nitrate.TestCase(name="Test Case {0}".format(i+1),\
-                    category=nitrate.Category(category),\
-                    product=product,\
-                    summary="Test Case {0}".format(i+1),\
-                    status=nitrate.CaseStatus(casestatus))
-            case.update()
+            case = nitrate.TestCase(name="Test Case {0}".format(case_count+1),
+                    category=CATEGORY.name,
+                    product=PRODUCT.name,
+                    summary="Test Case {0}".format(case_count+1),
+                    status=CASESTATUS)
             # Link with TestPlan
-            nitrate.TestCase(case.id).testplans._add(
-                    [nitrate.TestPlan(tplan.id)])
+            nitrate.TestCase(case.id).testplans._add([testplan])
             # Add a tag
-            nitrate.TestCase(case.id).tags._add([TAGS[random.randint(0,4)]])
-            # Add bug
-            nitrate.TestCase(case.id).bugs.add(nitrate.Bug(bug))
-        for r in range(0, options.runs):
+            nitrate.TestCase(case.id).tags._add(random.choice(TAGS))
+        for run_count in range(0, options.runs):
             # Create runs
-            run = nitrate.TestRun(name="Test Run {0}".format(i+1),\
-                    testplan=nitrate.TestPlan(tplan.id),\
-                    build=nitrate.Build(build),\
-                    product=product, summary="Test Run",\
-                    version=nitrate.Version(product_version).name)
+            run = nitrate.TestRun(name="Test Run {0}".format(run_count+1),
+                    testplan=testplan.id,
+                    build=BUILD.name,
+                    product=PRODUCT.name, summary="Test Run",
+                    version=VERSION.name)
